f72b6fe403c3e91af4751e04590c79ac2218adbe
Lin Desmit <lin.desmit@student.kuleuven.com>
2026-02-24 17:48:55 +0100

    slide 4 en 5 app + requirements

--- figma.py
+++ figma.py
@@ -80,14 +80,14 @@ img.show()
 
 ################################################
 #SLIDE 4
-img = Image.open("slide4.png")
+img = Image.open("app_slides/slide4.png")
 draw = ImageDraw.Draw(img)
 
 width, height = img.size
 
 learning_age = 27  # dynamisch
 
-font = ImageFont.truetype("arialbd.ttf", 400)
+font = ImageFont.truetype("arialbd.ttf", 200)
 
 # positie ongeveer waar jouw 67 stond
 x = width / 2
@@ -107,5 +107,55 @@ img.show()
 #########################################
 #slide5
 
+learner = "REPETITION\nBUILDER"
+#"FAST\nBURNER"
+#"STEADY\nRETAINER"  #dynamish
+
+description1= "YOU IGNITE FAST.\nYOU GRASP CONCEPTS\nQUICKER THAN MOST"
+description2= "YOU START QUIET.\nYOU GROW STRONGER\nEVERY PRACTICE"
+description3= "YOU KEEP GOING\nYOU REMEMBER\nWHAT'S IMPORTANT"
+
+if learner == "REPETITION\nBUILDER":
+    description = description2
+elif learner == "FAST\nBURNER":
+    description = description1
+elif learner == "STEADY\nRETAINER":
+    description = description3
+else:
+    description = "UNKNOWN PROFILE"
+
+img = Image.open("app_slides/slide5.png")
+draw = ImageDraw.Draw(img)
+
+width, height = img.size
+
+# Fonts
+font_title = ImageFont.truetype("arialbd.ttf", int(height*0.060))
+font_desc = ImageFont.truetype("arialbd.ttf", int(height*0.025))
+
+# ---- TITLE ----
+draw.text(
+    (width/2+62, height*0.35),   # positie midden boven duo
+    learner,
+    fill="#63E000",
+    font=font_title,
+    anchor="mm", 
+    align="center"
+)
+
+# ---- DESCRIPTION ----
+draw.text(
+    (width/2+60, height*0.51),   # onder duo
+    description,
+    fill="white",
+    font=font_desc,
+    anchor="mm",
+    align="center"
+)
+
+output_folder = "app_slides_update"
+output_path = os.path.join(output_folder, "slide_dynamic.png")
+img.save(output_path)
+img.show()
 
 
